0704b4de8b7c1bed584df2e624c77bf7a421d764
RosileneMoufer <rosilenemoufer@gmail.com>
2022-10-13 21:14:10 -0300

    first version

---
+++ data/data.js
@@ -0,0 +1,60 @@
+var data = [
+    {
+        "date" : "2009",
+        "title" : "Ensino médio e técnico",
+        "description" : "Ingressei no Técnico-Integrado em Telecomunicações pelo Instituto Federal de Educação, Ciência e Tecnologia do Ceará (IFCE), campus Fortaleza - CE.",
+    },
+    {
+        "date" : "2011", 
+        "title" : "Primeiras linhas de código com as linguagens HTML, CSS e C",
+        "description" : "Iniciei um curso onde comecei meus estudos em HTML, CSS. No IFCE, tive introdução à linguagem C, desenvolvendo programas simples."
+    },
+    {
+        "date" : "2012", 
+        "title" : "Primeiro contato com Android Studio",
+        "description" : "Comecei a estudar desenvolvimento de aplicativo mobile para Android com Java e o Android Studio."
+    },
+    {
+        "date" : "2013",
+        "title" : "Conclusão de curso",
+        "description" : "Formada no Técnico-Integrado em Telecomunicações pelo IFCE.",
+    },
+    {
+        "date" : "2014",
+        "title" : "Ingresso na Universidade",
+        "description" : `Iniciei o curso de Ciência da Computação pela Universidade Estadual do Ceará (UECE). Lá pude desenvolver projetos simples em C, Java com Eclipse e NetBeans.
+        Obtive conhecimento sobre o básico de Estrutura de Dados e segui utilizando HTML e CSS quando entrei, nesse mesmo ano, para a Empresa Júnior Acens.
+        Nesse ano, dei início aos estudos de JavaScript.`
+    },
+    {
+        "date" : "2016", 
+        "title" : "Ingresso na Universidade Federal do Ceará (UFC)",
+        "description" : `Comecei uma nova graduação no curso de Sistemas e Mídias Digitais. Como projetos de faculdade, desenvolvi dois jogos, um no Stencyl e o outro na Unity (2D).
+        Também utilizamos o Processing para desenvolver projetos. Você pode conferir os projetos que desenvolvi em Processing na seção Portfólio.`
+    },
+    {
+        "date" : "2017", 
+        "title" : "Bolsista voluntária",
+        "description" : "Bolsista do Programa Institucional de Bolsas de Iniciação Científica - PIBIC."
+    },
+    {
+        "date" : "2018", 
+        "title" : "Banco de Dados",
+        "description" : "Desenvolvimento de aplicativos desktop em Java e Postgree."
+    },
+    {
+        "date" : "2019", 
+        "title" : "Conclusão de curso",
+        "description" : "Graduada em Sistemas e Mídias Digitais pela UFC."
+    },
+    {
+        "date" : "2020", 
+        "title" : "Nova linguagem de programação",
+        "description" : "Dei início aos estudos de Flutter, desenvolvendo aplicativos mobile. Utilizando Provider (gerenciamento de estado), Streambuilder, ligação com banco de dados Firebase."
+    },
+    {
+        "date" : "2022", 
+        "title" : "Novas aventuras",
+        "description" : "Agora estou estudando React Native. Ainda no início."
+    },
+]
